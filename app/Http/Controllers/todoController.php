fcb21a339d90955cf8e24857c1c8c99449938f68
carlafaes <carlafaes18@hotmail.com>
2022-05-27 20:55:48 -0300

    todos

---
+++ app/Http/Controllers/todoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class todoController extends Controller
+{
+    /**
+     * index para mostrar todos los todos
+     * store para guardar un todo
+     * update para actualizar un todo
+     * destroy para eliminar un todo
+     * edit para mostrar el formulario de edicion
+     */
+    
+}
