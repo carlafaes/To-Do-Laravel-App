b42394ab9889241ad97d60bc18e95652a605a3bb
carlafaes <carlafaes18@hotmail.com>
2022-05-27 22:02:30 -0300

    alert

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -5,6 +5,18 @@
     <form action="{{ route('todos') }}" method="POST">
     <!-- cross site request forgery     -->
     @csrf 
+
+    @if (session('success'))
+        <h6 class="alert alert-success">
+            {{session('success')}}
+        </h6>
+    @endif
+
+    @error('title')
+    <h6 class="alert alert-danger">
+            {{$message}}
+        </h6>
+    @enderror
     <div class="mb-3">
         <label for="title" class="form-label">Task Title</label>
         <input type="text" name="title" class="form-control" >
